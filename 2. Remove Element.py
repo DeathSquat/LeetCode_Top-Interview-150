0d3c2f5d14aec8ad8d5943b85acc1b4271ff5f1a
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-02-19 00:31:32 +0530

    2. Remove Element

---
+++ 2. Remove Element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # Initialize a pointer to track the position for elements not equal to val
+        pointer = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[pointer] = nums[i]
+                pointer += 1
+        
+        return pointer
