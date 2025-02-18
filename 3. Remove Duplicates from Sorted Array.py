a69aafa7bf75faf794ceded79f5857ba87eb21d1
Trigger <89197100+DeathSquat@users.noreply.github.com>
2025-02-19 00:31:58 +0530

    3. Remove Duplicates from Sorted Array

---
+++ 3. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        # Initialize a pointer for the position of unique elements
+        unique_pos = 0
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[unique_pos]:
+                unique_pos += 1
+                nums[unique_pos] = nums[i]
+        
+        return unique_pos + 1
